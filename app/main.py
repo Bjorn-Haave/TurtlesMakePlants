c08c9f7fa9e673c64025e5a783be49ef5688b58b
Bjorn Haave <bjornhaave@yahoo.ca>
2020-04-12 21:38:19 -0600

    Made basic environment to test turtle

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 import turtle
+from math import pi
 
 class PlantTurtle:
-    def __init__(self):
+    def __init__(self, x, y):
         self.health = 1
+        self.decay = 0.5
         self.turtle = turtle.Turtle()
+        self.turtle.penup()
+        self.turtle.goto(x, y)
+        self.turtle.pendown()
         #TODO
         
     def step(self):
-        self.turtle.forward(10)
+        self.move(50 * self.health)
+        self.rotate_relative(20)
+        self.health = self.health * self.decay
         #TODO
         
     def place_at(self, x, y):
@@ -15,16 +22,13 @@ class PlantTurtle:
         #TODO
         
     def rotate_relative(self, theta):
-        pass
-        #TODO
+        self.turtle.left(theta)
     
     def rotate_abs(self, theta):
-        pass
-        #TODO
+        self.turtle.setheading(theta)
         
     def move(self, dist):
-        pass
-        #TODO
+        self.turtle.forward(dist)
         
     def update(self):
         pass
@@ -32,9 +36,12 @@ class PlantTurtle:
         
 def main():
     s = turtle.getscreen()
-    my_turtle = PlantTurtle()
-    my_turtle.step()
-    input()
+    s.setworldcoordinates(-100, 0, 100, 200)
+    my_turtle = PlantTurtle(0,0)
+    my_turtle.rotate_abs(90)
+    for i in range(5):
+        my_turtle.step()
+        input()
     
 if __name__ == "__main__":
     main()
