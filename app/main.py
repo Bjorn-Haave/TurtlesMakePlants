a29473ce4260e54a74b41cc0ce10af478e33a324
Bjorn Haave <bjornhaave@yahoo.ca>
2020-04-17 20:42:38 -0600

    Added logic for turtle copying and branching

--- app/main.py
+++ app/main.py
@@ -7,21 +7,28 @@ class Flock:
         self.flock = []
         
         self.screen.setworldcoordinates(-100, 0, 100, 200)
+        
         my_turtle = PlantTurtle(0,0)
         my_turtle.rotate_abs(90)
         self.flock.append(my_turtle)
         
     def step(self):
+        new_flock = []
         for t in self.flock:
-            t.step()
-        #TODO
+            new_flock += t.step()
+        else:
+            self.flock = new_flock
+            
 
 class PlantTurtle:
     def __init__(self, x, y):
         self.health = 1
-        self.decay = 0.5
+        self.decay = 0.7
         self.turtle = turtle.Turtle()
         
+        self.turtle.speed(0)
+        self.turtle.hideturtle()
+        # Is this code going to be the same for for place_at()
         self.turtle.penup()
         self.turtle.goto(x, y)
         self.turtle.pendown()
@@ -29,8 +36,14 @@ class PlantTurtle:
         
     def step(self):
         self.move(50 * self.health)
-        self.rotate_relative(20)
+        child = self.copy()
+        child.rotate_relative(-20)
+        child.health = child.health * child.decay
+
+        self.rotate_relative(45)
         self.health = self.health * self.decay
+
+        return [self, child]
         #TODO
         
     def place_at(self, x, y):
@@ -50,10 +63,18 @@ class PlantTurtle:
         pass
         #TODO
         
+    def copy(self):
+        other = PlantTurtle(self.turtle.xcor(), self.turtle.ycor())
+        other.health = self.health
+        other.decay = self.decay
+        other.turtle = self.turtle.clone()
+        return other
+        
 def main():
     my_flock = Flock()
     for i in range(5):
         my_flock.step()
+    input("Go Forward")
 
 
 if __name__ == "__main__":
